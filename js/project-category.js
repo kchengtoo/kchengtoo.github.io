a2814339645013dbf40a1870a09fbd6e3aeae1bd
Kevin Cheng <kevincheng2021@u.northwestern.edu>
2026-05-16 23:43:42 -0400

    Fixing project-category.js

--- js/project-category.js
+++ js/project-category.js
@@ -14,6 +14,7 @@ $(document).ready(function() {
 	// else {
 	// 	console.log("ERROR");
 	// }
+	console.log(currentLocation);
 	if (currentLocation.includes("index")) {
 		categoryName = "featured-work";
 		displayProjectLinks(designProjects);
